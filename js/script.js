444db5c16dbdd21abde55c03c3810ee1d346d9c4
Amanda Gxagxa <amandagxagxa@gmail.com>
2018-08-17 14:54:43 +0200

    commiting the matching colors

--- js/script.js
+++ js/script.js
@@ -5,20 +5,32 @@ function getRandomNum() {
   return Math.floor(Math.random() * 10);
 
 }
+  var nums = document.querySelectorAll(".nums");
+  function addTwo() {
+    nums[0].innerHTML === nums[1].innerHTML
+  }
 
 function showNums() {
-  var nums = document.querySelectorAll(".nums");
+
   var i;
 
   for (var i = 0; i < nums.length; i++) {
       nums[i].innerHTML = getRandomNum();
   }
 
+  if (nums[0].innerHTML === nums[1].innerHTML){
+    nums[0].classList.add("matchedColor");
+    nums[1].classList.add("matchedColor");
+  }
+  else if ( nums[1].innerHTML === nums[2].innerHTML) {
+    nums[1].classList.add("matchedColor");
+    nums[2].classList.add("matchedColor");
+  }
+  if (addTwo() == nums[2].innerHTML) {
+    nums[0].classList.add("doubleMaches");
+    nums[1].classList.add("doubleMaches");
+    nums[2].classList.add("doubleMaches");
+  }
+
 }
-
 showNums(getRandomNum());
-
-
-// function showNombers() {
-//
-// }
