7a5e7ee7baa7879dd57e812fae1629f881e363cf
Amanda Gxagxa <amandagxagxa@gmail.com>
2018-08-17 15:23:31 +0200

    commiting the matching message

--- js/script.js
+++ js/script.js
@@ -5,24 +5,31 @@ function getRandomNum() {
   return Math.floor(Math.random() * 10);
 
 }
-  var nums = document.querySelectorAll(".nums");
-  function addTwo() {
-    nums[0].innerHTML === nums[1].innerHTML
-  }
+
+let one = document.querySelector(".one")
+var nums = document.querySelectorAll(".nums");
+
+function addTwo() {
+  nums[0].innerHTML === nums[1].innerHTML
+}
 
 function showNums() {
-
+    // one = "";
   var i;
 
   for (var i = 0; i < nums.length; i++) {
-      nums[i].innerHTML = getRandomNum();
+    nums[i].innerHTML = getRandomNum();
   }
 
-  if (nums[0].innerHTML === nums[1].innerHTML){
+  if (nums[0].innerHTML === nums[1].innerHTML) {
+    // alert("Matche found !")
+    one.innerHTML = "Matche found !"
     nums[0].classList.add("matchedColor");
     nums[1].classList.add("matchedColor");
-  }
-  else if ( nums[1].innerHTML === nums[2].innerHTML) {
+
+  } else if (nums[1].innerHTML === nums[2].innerHTML) {
+    // alert("Matche found !")
+    one.innerHTML = "Matche found !"
     nums[1].classList.add("matchedColor");
     nums[2].classList.add("matchedColor");
   }
