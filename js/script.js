65a88fdbd774bc6b895100f785c055ea7deda267
Amanda Gxagxa <amandagxagxa@gmail.com>
2018-08-17 15:55:02 +0200

    commiting the matching message

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function getRandomNum() {
   return Math.floor(Math.random() * 10);
 
 }
-
+let btnElem = document.querySelector(".button")
 let one = document.querySelector(".one")
 var nums = document.querySelectorAll(".nums");
 
@@ -14,7 +14,7 @@ function addTwo() {
 }
 
 function showNums() {
-    // one = "";
+     // 4one = "";
   var i;
 
   for (var i = 0; i < nums.length; i++) {
@@ -24,20 +24,29 @@ function showNums() {
   if (nums[0].innerHTML === nums[1].innerHTML) {
     // alert("Matche found !")
     one.innerHTML = "Matche found !"
-    nums[0].classList.add("matchedColor");
-    nums[1].classList.add("matchedColor");
+    nums[0].style.color = "green";
+    nums[1].style.color = "green";
 
-  } else if (nums[1].innerHTML === nums[2].innerHTML) {
+  }
+  if (nums[1].innerHTML === nums[2].innerHTML) {
     // alert("Matche found !")
     one.innerHTML = "Matche found !"
-    nums[1].classList.add("matchedColor");
-    nums[2].classList.add("matchedColor");
+
+    nums[1].style.color = "green";
+    nums[2].style.color = "green";
+
+    // totalFieldSettings.style.fontWeight = "bold";
   }
-  if (addTwo() == nums[2].innerHTML) {
-    nums[0].classList.add("doubleMaches");
-    nums[1].classList.add("doubleMaches");
-    nums[2].classList.add("doubleMaches");
-  }
+  // if (addTwo() == nums[2].innerHTML) {
+  //   nums[0].classList.add("doubleMaches");
+  //   nums[1].classList.add("doubleMaches");
+  //   nums[2].classList.add("doubleMaches");
+  // }
 
 }
 showNums(getRandomNum());
+
+btnElem.addEventListener("click" , function() {
+  showNums(getRandomNum());
+
+})
